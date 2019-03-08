97c63a9721962aa7e71f8af08de6cffc419d5258
Ilario Pierbattista <ilario.pierbattista@facile.it>
2019-03-08 12:54:29 +0100

    done

--- typescript/src/TennisGame1.ts
+++ typescript/src/TennisGame1.ts
@@ -1,13 +1,10 @@
 import { TennisGame } from './TennisGame';
-import { Option } from 'fp-ts/lib/Option'
+import { Option, some, none } from 'fp-ts/lib/Option'
+import { Predicate, identity } from 'fp-ts/lib/function';
+import { max } from 'fp-ts/lib/Foldable2v';
 
 export namespace TennisGame1 {
   export type player = 'player1' | 'player2';
-
-  export type PlayerScore = {
-    player: player,
-    score: scoreType,
-  }
 
   export type scoreType =
     | {
@@ -40,14 +37,8 @@ class GameState {
 
   public static init(): GameState {
     return new GameState({
-      player1: {
-        name: 'Love',
-        value: 0
-      },
-      player2: {
-        name: 'Love',
-        value: 0
-      }
+      player1: love(),
+      player2: love()
     });
   }
 
@@ -67,72 +58,129 @@ class GameState {
   }
 }
 
-declare function winner(state: GameState): TennisGame1.player;
-declare function incrementScore(score: TennisGame1.scoreType): TennisGame1.scoreType;
-declare function playerFromString(name: string): TennisGame1.player;
+function love(): TennisGame1.scoreType {
+  return {
+    name: 'Love',
+    value: 0
+  }
+}
+function fifteen(): TennisGame1.scoreType {
+  return {
+    name: 'Fifteen',
+    value: 1
+  }
+}
+function thirty(): TennisGame1.scoreType {
+  return {
+    name: 'Thirty',
+    value: 2
+  }
+}
+function forty(): TennisGame1.scoreType {
+  return {
+    name: 'Forty',
+    value: 3
+  }
+}
+
+function deuce(v: number): TennisGame1.scoreType {
+  return {
+    name: 'Deuce',
+    value: v >= 4 ? v : 4
+  }
+}
+
+const parita: Predicate<GameState> = (game: GameState) => game.state.player1.value === game.state.player2.value;
+const isWinnable: Predicate<GameState> = (game: GameState) => game.state.player1.value >= 4 || game.state.player2.value >= 4;
+
+function playerMaxScore(game: GameState): TennisGame1.player {
+  return game.state.player1.value > game.state.player2.value
+    ? 'player1'
+    : 'player2';
+}
+
+function winner(game: GameState): Option<TennisGame1.player> {
+  if (isWinnable(game) && Math.abs(game.state.player1.value - game.state.player2.value) >= 2) {
+    return some(playerMaxScore(game));
+  }
+
+  return none;
+}
+
+function advantage(game: GameState): Option<TennisGame1.player> {
+  if (isWinnable(game) && Math.abs(game.state.player1.value - game.state.player2.value) === 1) {
+    return some(playerMaxScore(game));
+  }
+
+  return none;
+}
+
+function scoreParita(game: GameState): Option<string> {
+  if (parita(game) && game.state.player1.value >= forty().value) {
+    return some('Deuce');
+  }
+
+  if (parita(game)) {
+    return some(`${game.state.player1.name}-All`)
+  }
+
+  return none;
+}
+
+function incrementScore(score: TennisGame1.scoreType): TennisGame1.scoreType {
+  switch (score.name) {
+    case 'Love':
+      return fifteen();
+    case 'Fifteen':
+      return thirty();
+    case 'Thirty':
+      return forty();
+    case 'Forty':
+      return deuce(4);
+    case 'Deuce':
+      return deuce(score.value + 1);
+  }
+}
+
+function playerFromString(name: string): Option<TennisGame1.player> {
+  switch (name) {
+    case 'player1':
+      return some<TennisGame1.player>('player1');
+    case 'player2':
+      return some<TennisGame1.player>('player2');
+    default:
+      return none;
+  }
+}
 
 export class TennisGame1 implements TennisGame {
-  private m_score1: number = 0;
-  private m_score2: number = 0;
-  private state: GameState = GameState.init();
+  private game: GameState = GameState.init();
 
   constructor(readonly player1Name: string, readonly player2Name: string) {
   }
 
   wonPoint(playerName: string): void {
-    if (playerName === 'player1') {
-      this.m_score1 += 1;
-    }
-    else {
-      this.m_score2 += 1;
-    }
+    this.game = playerFromString(playerName)
+      .fold(
+        this.game,
+        p => this.game.point(p)
+      )
   }
 
   getScore(): string {
-    let score: string = '';
-    let tempScore: number = 0;
-    if (this.m_score1 === this.m_score2) {
-      switch (this.m_score1) {
-        case 0:
-          score = 'Love-All';
-          break;
-        case 1:
-          score = 'Fifteen-All';
-          break;
-        case 2:
-          score = 'Thirty-All';
-          break;
-        default:
-          score = 'Deuce';
-          break;
-      }
-    } else if (this.m_score1 >= 4 || this.m_score2 >= 4) {
-      const minusResult: number = this.m_score1 - this.m_score2;
-      if (minusResult === 1) score = 'Advantage player1';
-      else if (minusResult === -1) score = 'Advantage player2';
-      else if (minusResult >= 2) score = 'Win for player1';
-      else score = 'Win for player2';
-    } else {
-      for (let i = 1; i < 3; i++) {
-        if (i === 1) tempScore = this.m_score1;
-        else { score += '-'; tempScore = this.m_score2; }
-        switch (tempScore) {
-          case 0:
-            score += 'Love';
-            break;
-          case 1:
-            score += 'Fifteen';
-            break;
-          case 2:
-            score += 'Thirty';
-            break;
-          case 3:
-            score += 'Forty';
-            break;
-        }
-      }
-    }
+    const printers: ((g: GameState) => Option<string>)[] = [
+      scoreParita,
+      g => winner(g).chain(w => some(`Win for ${w}`)),
+      g => advantage(g).chain(a => some(`Advantage ${a}`))
+    ];
 
-    return score;
+    return printers
+      .reduce(
+        (carry, printer) => {
+          return carry.alt(printer(this.game))
+        },
+        none as Option<string>
+      )
+      .getOrElse(`${this.game.state.player1.name}-${this.game.state.player2.name}`)
   }
 }
